96fbc632c86927edc6335fecd54e5e61a527682f
Boonwarat <Boonwarat074@gmail.com>
2024-11-18 15:37:27 +0700

    copy and make another api sum.py work the same just change name

---
+++ routers/Summarize.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter, status
+from internal.SumService import LLMRequest
+from models.SumModels import SumRequest
+import utils.response as res
+import os
+
+from dotenv import load_dotenv
+load_dotenv(dotenv_path="E:\Ai\edusaig\edusaig-ai\.env")
+
+router = APIRouter()
+
+print("chck")
+
+url = os.getenv("SAIG_LLM_URL")
+model_name = "meta-llama/Meta-Llama-3.1-8B-Instruct"
+llm_request = LLMRequest(url=url, model=model_name)
+
+@router.post("/qa", status_code=status.HTTP_200_OK)
+async def qa(body: SumRequest):
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": body.content},
+    ]
+    
+    response = llm_request.send_request(messages)
+    
+    if response:
+        return res.success_response_status(
+            status=status.HTTP_200_OK,
+            message="Generate comment successful",
+            data=response
+        )
+    else:
+        return res.error_response_status(
+            status=status.HTTP_400_BAD_REQUEST,
+            message="Failed to get a valid response from LLM."
+        )
