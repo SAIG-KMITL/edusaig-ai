4b202bb5bc01e6fdd025ec18d52cac67c684267b
BikiniGordon <gatchanminecraft@gmail.com>
2024-11-19 22:04:01 +0700

    Fix - edit prefix
    edit from ai/generate-pretest/ to /generate-pretest/

--- routers/pre_test.py
+++ routers/pre_test.py
@@ -15,7 +15,7 @@ url = os.getenv("SAIG_LLM_URL")
 model_name = "meta-llama/Meta-Llama-3.1-8B-Instruct"
 llm_request = LLMRequest(url=url, model=model_name)
 
-@router.post("/ai/generate-pretest/", status_code=status.HTTP_200_OK)
+@router.post("/generate-pretest/", status_code=status.HTTP_200_OK)
 async def qa(body: PreTestRequest):
     messages = [
         {
